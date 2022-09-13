d12960e91e1a4342051a44ae03266236456cbaf2
Paulo <paulo.batista025@outlook.com>
2022-09-13 19:29:18 -0300

    primeira versao

--- sca/settings.py
+++ sca/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6u765%n)s-6sda*-@vyys2*f+hodp^@--ge%s2n%7yydfrb!bx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
